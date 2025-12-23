e27b4cb0d65bb6270a0dd9b9278a029cb14a21df
muhamadgumilang <muhamadgumilang68@gmail.com>
2025-12-23 09:11:39 +0700

    Initial commit

---
+++ pkl-internal-26/routes/web.php
@@ -0,0 +1,23 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+// ================================================
+// TUGAS: Tambahkan route baru di bawah ini
+// ================================================
+
+Route::get('/tentang', function () {
+    // ================================================
+    // Route::get() = Tangani HTTP GET request
+    // '/tentang'   = URL yang akan dihandle
+    // function     = Kode yang dijalankan saat URL diakses
+    // ================================================
+
+    return view('tentang');
+    // ↑ return view('tentang') = Tampilkan file tentang.blade.php
+    // ↑ Laravel akan mencari di: resources/views/tentang.blade.php
+});
